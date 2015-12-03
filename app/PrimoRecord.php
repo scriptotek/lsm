69d88eef1b4358e0f6f33e134b7f7b00165cc944
Dan Michael O. Heggø <danmichaelo@gmail.com>
2015-12-03 09:31:52 +0000

    Add more subject vocabularies to PrimoRecord

--- app/PrimoRecord.php
+++ app/PrimoRecord.php
@@ -90,6 +90,9 @@ class PrimoRecord implements \JsonSerializable
 
         // @TODO get indices from config
         $this->full['subjects']['realfagstermer'] = $this->extractArray($record, './p:search/p:lsr20');
+        $this->full['subjects']['humord'] = $this->extractArray($record, './p:search/p:lsr14');
+        $this->full['subjects']['tekord'] = $this->extractArray($record, './p:search/p:lsr12');
+        $this->full['subjects']['mrtermer'] = $this->extractArray($record, './p:search/p:lsr19');
 
         return $this;
     }
