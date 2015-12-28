3146c1346b6870a3e627f59f721a1e5f58dd8f1f
Dan Michael O. Heggø <danmichaelo@gmail.com>
2015-12-28 15:38:57 +0000

    Add PrimoRecord test

---
+++ tests/PrimoRecordTest.php
@@ -0,0 +1,37 @@
+<?php
+
+use App\PrimoRecord;
+use BCLib\PrimoServices\DeepLink;
+use Danmichaelo\QuiteSimpleXMLElement\QuiteSimpleXMLElement;
+
+class PrimoRecordTest extends TestCase
+{
+	public function loadPrimoRecord($filename)
+	{
+		$xml = file_get_contents(__DIR__ . '/data/' . $filename);
+
+        $root = new QuiteSimpleXMLElement($xml);
+        $root->registerXPathNamespace('s', 'http://www.exlibrisgroup.com/xsd/jaguar/search');
+        $root->registerXPathNamespace('p', 'http://www.exlibrisgroup.com/xsd/primo/primo_nm_bib');
+
+        $doc = $root->first('//s:DOC');
+        $dl = new DeepLink('','');
+        return PrimoRecord::make($doc, $dl, false, [])->toArray(true);
+	}
+
+    public function testKeywords()
+    {
+    	$record = $this->loadPrimoRecord('PrimoRecordTest1.xml');
+
+        $this->assertSetsEqual(
+            ['engelsk', 'toefl', 'språktester', 'testen', 'språkbruk', 'øvingsbøker'],
+            array_get($record, 'subjects.keywords')
+        );
+
+        $this->assertSetsEqual(
+            [],
+            array_get($record, 'subjects.subject')
+        );
+
+    }
+}
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -11,4 +11,21 @@ class TestCase extends Laravel\Lumen\Testing\TestCase
     {
         return require __DIR__.'/../bootstrap/app.php';
     }
+
+    /**
+	 * Assert that two arrays are equal. This helper method will sort the two arrays before comparing them if
+	 * necessary. This only works for one-dimensional arrays, if you need multi-dimension support, you will
+	 * have to iterate through the dimensions yourself.
+	 * @param array $expected the expected array
+	 * @param array $actual the actual array
+	 */
+	protected function assertSetsEqual(array $expected, array $actual) {
+	    // check length first
+	    // $this->assertEquals(count($expected), count($actual), 'Failed to assert that two arrays have the same length.');
+
+        $this->assertTrue(sort($expected), 'Failed to sort array.');
+        $this->assertTrue(sort($actual), 'Failed to sort array.');
+
+	    $this->assertEquals($expected, $actual);
+	}
 }
