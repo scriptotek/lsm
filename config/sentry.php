1dea19a33fdb778e537a08b17c826a16c4a276e3
Dan Michael O. Heggø <danmichaelo@gmail.com>
2019-05-06 21:41:18 +0200

    Update deps
    Update to use PSR-17/18 compatible libraries (php-http/guzzle6-adapter
    2.0, http-interop/http-factory-guzzle 1.0, etc.)

--- config/sentry.php
+++ config/sentry.php
@@ -7,8 +7,8 @@ return [
     'release' => trim(exec('git log --pretty="%h" -n1 HEAD')),
 
     // Capture bindings on SQL queries
-    'breadcrumbs.sql_bindings' => true,
-
-    // Capture default user context
-    'user_context' => true,
+    'breadcrumbs' => [
+        // Capture bindings on SQL queries logged in breadcrumbs
+        'sql_bindings' => true,
+    ],
 ];
