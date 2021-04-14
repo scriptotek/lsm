92e26900c5e9cf7b11ba5a3107dc6fd356245f1d
Dan Michael O. Heggø <danmichaelo@gmail.com>
2021-04-14 14:44:36 +0200

    Update alma config

--- config/alma.php
+++ config/alma.php
@@ -15,8 +15,20 @@ return [
     |--------------------------------------------------------------------------
     */
     'iz' => [
+        // API key for institution zone
         'key' => env('ALMA_IZ_KEY'),
+
+        // SRU URL for institution zone
         'sru' => env('ALMA_IZ_SRU_URL'),
+
+        // Base URL for institution zone. This only needs to be specified if you
+        // use a proxy or other non-standard URL.
+        'baseUrl' => 'https://gw-uio.intark.uh-it.no/alma/v1',
+
+        // Optional list of extra headers to send with each request.
+        'extraHeaders' => [
+            'X-Gravitee-Api-Key' => env('ALMA_IZ_GRAVITEE_KEY'),
+        ],
     ],
 
     /*
@@ -25,8 +37,19 @@ return [
     |--------------------------------------------------------------------------
     */
     'nz' => [
+        // API key for institution zone
         'key' => env('ALMA_NZ_KEY'),
-        'sru' => env('ALMA_NZ_SRU_URL'),
-    ],
 
+        // SRU URL for institution zone
+        'sru' => env('ALMA_NZ_SRU_URL'),
+
+        // Base URL for institution zone. This only needs to be specified if you
+        // use a proxy or other non-standard URL.
+        'baseUrl' => 'https://gw-uio.intark.uh-it.no/alma/v1',
+
+        // Optional list of extra headers to send with each request.
+        'extraHeaders' => [
+            'X-Gravitee-Api-Key' => env('ALMA_NZ_GRAVITEE_KEY'),
+        ],
+    ],
 ];
